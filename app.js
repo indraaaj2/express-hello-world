8a29eb40db58440f818c0cc740dbc3fd79d9425f
indraaaj2 <eertidelhi@gmail.com>
2026-01-08 14:31:45 +0530

    cache increased

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // or restrict to your domain
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Max-Age", "7200");
   next();
 });
 
