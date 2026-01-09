ce954c7cd25e8f4360652e26d076de0ecf06286e
indraaaj2 <eertidelhi@gmail.com>
2026-01-09 15:56:19 +0530

    asd

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // or restrict to your domain
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Max-Age", "7200");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Max-Age", "86400");
   next();
 });
 
